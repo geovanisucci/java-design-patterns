e7e73e144c7f11c5037948add83fb7f08bc70e3d
geovanis <geovanis@ciandt.com>
2022-02-03 17:24:11 -0300

    pattern observer implemented

--- loja/src/main/java/br/com/loja/TestesPedidos.java
+++ loja/src/main/java/br/com/loja/TestesPedidos.java
@@ -4,9 +4,12 @@ import br.com.loja.orcamento.Orcamento;
 import br.com.loja.pedido.GeraPedido;
 import br.com.loja.pedido.GeraPedidoHandler;
 import br.com.loja.pedido.Pedido;
+import br.com.loja.pedido.acao.EnviarEmailPedido;
+import br.com.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class TestesPedidos {
 
@@ -18,7 +21,9 @@ public class TestesPedidos {
 
         GeraPedido geraPedido = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
 
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependências*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new SalvarPedidoNoBancoDeDados(),
+                new EnviarEmailPedido()));
+
         handler.execute(geraPedido);
     }
 }
--- loja/src/main/java/br/com/loja/pedido/GeraPedidoHandler.java
+++ loja/src/main/java/br/com/loja/pedido/GeraPedidoHandler.java
@@ -1,20 +1,27 @@
 package br.com.loja.pedido;
 
 import br.com.loja.orcamento.Orcamento;
+import br.com.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.loja.pedido.acao.EnviarEmailPedido;
+import br.com.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
 
     //Construtor com injeção de dependências: Repository, Service, etc
 
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
+
     public void execute(GeraPedido dados){
         Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
-
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
-
-        System.out.println("Salvar pedido no banco de dados");
-        System.out.println("Enviar email com dados do novo pedido");
+        acoes.forEach(a -> a.executarAcao(pedido));
     }
 
 }
