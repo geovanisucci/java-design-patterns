fd1b0a5733eadb6e98951c364a0ae78a7ed2ada7
geovanis <geovanis@ciandt.com>
2022-02-03 16:34:25 -0300

    pattern state implemented

---
+++ loja/src/main/java/br/com/loja/orcamento/situacao/Reprovado.java
@@ -0,0 +1,13 @@
+package br.com.loja.orcamento.situacao;
+
+import br.com.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class Reprovado extends SituacaoOrcamento{
+
+    @Override
+    public void finalizar(Orcamento orcamento) {
+        orcamento.setSituacao(new Finalizado());
+    }
+}
